62b8f459d720b273954e83a422a0440b01c98522
Feyza Kaymakci <feyza.kaymakci@hotmail.com>
2023-05-23 15:48:06 +0300

    23.05.2023

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { Create_User } from 'src/app/contracts/users/create_user';
+import { User } from 'src/app/entities/user';
+import { UserService } from 'src/app/services/common/models/user.service';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 @Component({
   selector: 'app-register',
@@ -8,11 +12,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit{
 
-  constructor(private formBuilder:FormBuilder) { }
+  constructor(private formBuilder:FormBuilder, private userService: UserService, private toastrService: CustomToastrService) { }
+
     frm:FormGroup;
+
   ngOnInit(): void {
+
     this.frm=this.formBuilder.group({ //Reactive olarak üzerinde çalışma yapacağımız formu modellemiş olduk. 
-      fullName:["", [  //İkinci dizide biz validation larımızı tanımlıyoruz.
+      fullName:["", [  //İkinci dizide validation larımızı tanımlıyoruz.
        Validators.required,
        Validators.maxLength(50),
        Validators.minLength(3)
@@ -31,13 +38,39 @@ export class RegisterComponent implements OnInit{
         Validators.required,
         Validators.minLength(6)
       ]],
-      confirmPassword:[""]
+      confirmPassword:["", [
+        Validators.required,
+        Validators.minLength(6)
+      ]]
+    },{
+      validators:(group:AbstractControl):ValidationErrors | null=>{
+        let password =group.get("password").value;
+        let confirmPassword=group.get("confirmPassword").value;
+        return password===confirmPassword ? null:{notSame:true};
+      }
     })
-  }
+    }
+  
   get component(){
     return this.frm.controls;
   }
-  onSubmit(data:any){
-
-  }
+  submitted:boolean=false;
+  async onSubmit(user:User){
+    this.submitted=true;
+    
+    debugger;
+    if(this.frm.invalid)
+    return;
+    const result: Create_User = await this.userService.create(user);
+    if (result.succeeded)
+      this.toastrService.message(result.message, "User registration successful", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      })
+    else
+      this.toastrService.message(result.message, "Error", {
+        messageType: ToastrMessageType.Error,
+        position: ToastrPosition.TopRight
+      })
+    }
 }

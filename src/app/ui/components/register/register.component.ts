caff7411d5e9754903daec0de033241616b620ee
Feyza Kaymakci <feyza.kaymakci@hotmail.com>
2023-05-24 10:58:33 +0300

    24.05.2023

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { BaseComponent } from 'src/app/base/base.component';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { User } from 'src/app/entities/user';
 import { UserService } from 'src/app/services/common/models/user.service';
@@ -10,10 +12,11 @@ import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent extends BaseComponent implements OnInit{
 
-  constructor(private formBuilder:FormBuilder, private userService: UserService, private toastrService: CustomToastrService) { }
-
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private toastrService: CustomToastrService, spinner: NgxSpinnerService) {
+    super(spinner)
+  }
     frm:FormGroup;
 
   ngOnInit(): void {
@@ -58,15 +61,16 @@ export class RegisterComponent implements OnInit{
   async onSubmit(user:User){
     this.submitted=true;
     
-    debugger;
+
     if(this.frm.invalid)
     return;
     const result: Create_User = await this.userService.create(user);
     if (result.succeeded)
+    {
       this.toastrService.message(result.message, "User registration successful", {
         messageType: ToastrMessageType.Success,
         position: ToastrPosition.TopRight
-      })
+      })}
     else
       this.toastrService.message(result.message, "Error", {
         messageType: ToastrMessageType.Error,

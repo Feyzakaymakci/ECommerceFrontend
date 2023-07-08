3cac3ee079f735e610d1c098307febe822da364a
Feyza Kaymakci <feyza.kaymakci@hotmail.com>
2023-07-08 15:11:35 +0300

    08.07.2023

--- src/app/ui/components/products/list/list.component.ts
+++ src/app/ui/components/products/list/list.component.ts
@@ -4,16 +4,27 @@ import { BaseUrl } from '../../../../contracts/base_url';
 import { List_Product } from '../../../../contracts/list_product';
 import { FileService } from '../../../../services/common/models/file.service';
 import { ProductService } from '../../../../services/common/models/product.service';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { BasketService } from 'src/app/services/common/models/basket.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
+import { Create_Basket_Item } from 'src/app/contracts/basket/create_basket_item';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss']
 })
-export class ListComponent implements OnInit {
+export class ListComponent extends BaseComponent implements OnInit {
 
-  constructor(private productService: ProductService, private activatedRoute: ActivatedRoute, private fileService: FileService) { }
-
+  constructor(private productService: ProductService, 
+    private activatedRoute: ActivatedRoute, 
+    private fileService: FileService, 
+    private basketService: BasketService, 
+    spinner: NgxSpinnerService, 
+    private customToastrService: CustomToastrService) {
+    super(spinner)
+  }
   currentPageNo: number;
   totalProductCount: number;
   totalPageCount: number;
@@ -73,5 +84,16 @@ export class ListComponent implements OnInit {
     });
 
   }
-
+  async addToBasket(product: List_Product) {
+    this.showSpinner(SpinnerType.BallAtom);
+    let _basketItem: Create_Basket_Item = new Create_Basket_Item();
+    _basketItem.productId = product.id;
+    _basketItem.quantity = 1;
+    await this.basketService.add(_basketItem);
+    this.hideSpinner(SpinnerType.BallAtom);
+    this.customToastrService.message("Ürün sepete eklenmiştir.", "Sepete Eklendi", {
+      messageType: ToastrMessageType.Success,
+      position: ToastrPosition.TopRight
+    });
+  }
 }
